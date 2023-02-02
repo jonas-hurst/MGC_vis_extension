4b819f6c7e19a2b91e17dfe2134a130bc82d53c3
hurst <jonhur@web.de>
2023-02-02 17:21:51 +0100

    added basic websocket skeleton

--- scripts/content.js
+++ scripts/content.js
@@ -17,12 +17,28 @@ const dotstyle = [
 htmlElement.style = dotstyle.join(" ")
 
 if (confirm("do you want to connect to Server?")){
-    const websocketUrl = "";
-    const socket = new WebSocket(websocketUrl)
-    socket.onmessage = OnMessage
-}
-document.body.insertAdjacentElement("afterbegin", htmlElement)
 
-const OnMessage = (event) => {
-    console.log(event.data)
+    const websocketUrl = "ws://localhost:1234";
+    const socket = new WebSocket(websocketUrl)
+
+    // Show error message on error
+    socket.onerror = (event) => {
+        alert("Something went wrong connecting to server.")
+    }
+
+    // Add pointer to DOM if connection is open
+    socket.onopen = () => {
+        document.body.insertAdjacentElement("afterbegin", htmlElement)
+    }
+
+    // Remove pointer from DOM once connection closes
+    socket.onclose = () => {
+        htmlElement.remove();
+    }
+
+    // Process message from server
+    socket.onmessage = (event) => {
+        console.log(event.data);
+    }
+
 }
