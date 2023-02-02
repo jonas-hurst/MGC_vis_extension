cbcaed9f1d5b6f2e25939ac5dda4633010870d7c
hurst <jonhur@web.de>
2023-02-02 17:02:33 +0100

    restructure project

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,28 @@
 console.log("Hallo Welt!");
 
-document.body.innerHTML += "<h1>Title for Page</h1>";
+const htmlElement = document.createElement("div")
+htmlElement.className = "coarseDot"
+
+const dotstyle = [
+    "position: fixed;",
+    "top: 10px;",
+    "left: 10px;",
+    "pointer-events: none;",
+    "width: 50px;",
+    "height: 50px;",
+    "background-color: red;",
+    "display:inline-block;",
+    "border-radius:50%;",
+    "z-index: 99999",]
+htmlElement.style = dotstyle.join(" ")
+
+if (confirm("do you want to connect to Server?")){
+    const websocketUrl = "";
+    const socket = new WebSocket(websocketUrl)
+    socket.onmessage = OnMessage
+}
+document.body.insertAdjacentElement("afterbegin", htmlElement)
+
+const OnMessage = (event) => {
+    console.log(event.data)
+}
